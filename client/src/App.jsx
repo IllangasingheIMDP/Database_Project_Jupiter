2e9230f166a34462b4445b3d11f2b76e96534479
IllangasingheIMDP <157776370+IllangasingheIMDP@users.noreply.github.com>
2024-10-02 01:51:55 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import LoginForm from './pages/Login';
 import Home from './pages/Home';
