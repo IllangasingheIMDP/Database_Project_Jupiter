69b36cd193eceb63491289a8beeb53be4a6ad759
IllangasingheIMDP <157776370+IllangasingheIMDP@users.noreply.github.com>
2024-10-25 01:23:05 +0530

    customize leave allowences started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Manage_CustomField from './pages/Manage_Custom_Fields';
 import EditTableAttributes from './pages/Admin/Edit_Table_Attributes';
 import TableDetails from './pages/Admin/EditTable';
 import GenRepHR from './pages/HRManager/GenRepHR'
-
+import CustomizeLeaves from './pages/Admin/CustomizeLeaves';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Dashboard from './pages/Dashboard';
@@ -37,6 +37,8 @@ function App() {
     <Route path="/hrmanager-genReport" element={<ProtectedRoute allowedRoles={['HR Manager']}><GenRepHR /></ProtectedRoute>} />
     <Route path="/leaveRequest" element={<ProtectedRoute allowedRoles={['Employee','HR Manager']}><LeaveRequest></LeaveRequest></ProtectedRoute>} />
     <Route path="/approve-reject-leaves" element={<ProtectedRoute allowedRoles={['Employee','HR Manager']}><ManageEmployeeRequests></ManageEmployeeRequests></ProtectedRoute>} />
+    <Route path="/customizeleaves" element={<ProtectedRoute allowedRoles={['Admin User']}><CustomizeLeaves></CustomizeLeaves></ProtectedRoute>} />
+    
     </Routes>
     </Router>
 
