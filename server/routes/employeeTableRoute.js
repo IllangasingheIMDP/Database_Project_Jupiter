b5139229b3d604edd6945054007ef59dd7088e9a
Krishmalgg <krishmalgamage20021027@gmail.com>
2024-10-30 10:01:03 +0530

    set profile picture
    load profile picture from my sql data base path

--- server/routes/employeeTableRoute.js
+++ server/routes/employeeTableRoute.js
@@ -2,30 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const employeeController = require('../controllers/employeeController');
-const multer = require('multer');  // Import multer
-const { v4: uuidv4 } = require('uuid');  // Import uuid for unique image names
-const path = require('path');
-
-// Multer configuration for file upload
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Save images to the 'uploads' directory
-  },
-  filename: (req, file, cb) => {
-    const uniqueName = uuidv4(); // Generate a unique 36-character name
-    const ext = path.extname(file.originalname); // Get file extension
-    cb(null, `${uniqueName}${ext}`); // Save the file with the unique name + original extension
-  },
-});
-
-const upload = multer({ storage: storage });  // Define upload using multer
-
-
-
-
-
-
-
 
 
 
@@ -34,7 +10,7 @@ router.delete('/',authMiddleware(['Admin User',"HR Manager"]),employeeController
 router.put('/',authMiddleware(['Admin User',"HR Manager"]),employeeController.updateEmployeeData);
 router.post('/',authMiddleware(['Admin User',"HR Manager"]),employeeController.createNewEmployee);
 
-router.post('/add_employee',authMiddleware(['Admin User',"HR Manager"]), upload.single('picture'), employeeController.createNewEmployee);
+router.post('/add_employee',authMiddleware(['Admin User',"HR Manager"]), employeeController.createNewEmployee);
 
 router.get('/get_dropdown_options',authMiddleware(['Admin User',"HR Manager"]),employeeController.get_dropdown_options);
 router.post('/delete_employee/:nic',authMiddleware(['Admin User',"HR Manager"]),employeeController.deleteEmployee);
